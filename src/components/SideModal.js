fd18b5a287b6f87f28afb7ff7cb05d03b9a44fc2
halikrizwan <spotxchain@gmail.com>
2022-08-09 21:14:51 +0400

    added side model

--- src/components/SideModal.js
+++ src/components/SideModal.js
@@ -1,20 +1,33 @@
 import React,{ useState } from 'react'
+import downImg from '../assets/down.png'
 
 const SideModal = () => {
-    const [ getData, setData ] = useState(true);
+  const [ getData, setData ] = useState('1');
+
+  const goNext = () => {
+    setData('2')
+  }
   return (
    <div className='sideModal'>
-    {getData ?
+    {getData=='1' ?
     <div className='p-5'>
         <h1 className=''>Experience The Best Car Service In Ahamedabad</h1>
         <div className='mt-4'>Get instant quotes for your car service</div>
-        <div className='input mt-2'><span>Honda Amaze, Petrol</span></div>
+        <button className='input mt-2 w-100' onClick={goNext}>
+            <span>Honda Amaze, Petrol</span>
+            <img src={downImg} style={{width:20,height:20}}/>
+        </button>
         <div className='w-100 btn btn-danger mt-2'>CHECK PRICE FOR FREE</div>
         <div className='terms mt-2'>By continuing, you agree to GoMechanic's Terms & Conditions and Privacy Policy</div>
     </div>
     :
+    getData == '2' ?
     <div>
         Rizwan
+    </div>
+    :
+    <div>
+        Data
     </div>
     }
    </div>
